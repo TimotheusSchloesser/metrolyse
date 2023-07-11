e3663dcf1f7cdb10b657e66fbfe68d9739ef5cf0
Timmy Schuetz <timotheusschuetz@outlook.de>
2023-07-11 11:05:26 +0200

    generatedconfig.camke problem

---
+++ metrolyse/metrolyse/lib/control/start_stop_button.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import '../model/metronom_funktion.dart';
+
+bool isPlaying = false;
+MetronomFunctions metronomFunctions = MetronomFunctions();
+
+class StartStopBtn extends StatefulWidget {
+  const StartStopBtn({super.key});
+
+  @override
+  State<StartStopBtn> createState() => StartStopBtnState();
+}
+
+class StartStopBtnState extends State<StartStopBtn> {
+  @override
+  Widget build(BuildContext context) {
+    return playStopFunc();
+  }
+
+  IconButton playStopFunc() {
+    return IconButton(
+        icon: isPlaying
+            ? const Icon(Icons.play_circle_outline_outlined)
+            : const Icon(
+                Icons.stop_circle_outlined,
+              ),
+        onPressed: () {
+          setState(() {
+            // Here we changing the icon.
+            isPlaying = !isPlaying;
+            if (isPlaying) {
+              metronomFunctions.playClick();
+            } else {
+              clickplays();
+              stopClick();
+            }
+          });
+        });
+  }
+
+  clickplays() {
+    return isPlaying;
+  }
+}
